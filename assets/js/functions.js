656a54305cf296e634feaa0e8fd277eec5a4e782
Rodrigo <rodrigogv.1014@gmail.com>
2017-03-19 19:55:06 -0300

    Sunday touch

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,32 @@
-$( document ).ready(function() {
-
+$(function() {
+  smoothScroll(300);
+  tallerBelt();
   //
-
 });
+
+// smoothScroll function is applied from the document ready function
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top
+	        }, duration);
+	    }
+	});
+}
+
+function tallerBelt() {
+  $(".thumb-unit").click(function() {
+    $(".taller-belt").css('left', '-100%');
+    $(".info-taller").show();
+  });
+
+  $(".taller-return").click(function() {
+    $(".taller-belt").css('left', '0%');
+    $(".info-taller").hide(800);
+  });
+}
